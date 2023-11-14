cbd558d3bea07b0bb9d7643b0c8b095f18e349ec
WyattJ <jxq_ws@163.com>
2023-11-14 21:02:00 +0800

    完成打印标志

--- src/N_m3u8DL-RE/Column/DownloadStatusColumn.cs
+++ src/N_m3u8DL-RE/Column/DownloadStatusColumn.cs
@@ -39,7 +39,6 @@ namespace N_m3u8DL_RE.Column
                 var totalSize = speedContainer.SingleSegment ? (speedContainer.ResponseLength ?? 0) : (long)(size / (task.Value / task.MaxValue));
 
                 // todo 我的修改
-
                 Logger.Info($"<Status>: {GlobalUtil.FormatFileSize(size)}/{GlobalUtil.FormatFileSize(totalSize)}");
 
                 SizeDic[task.Id] = $"{GlobalUtil.FormatFileSize(size)}/{GlobalUtil.FormatFileSize(totalSize)}";
