063bde4860874410885b0d96594e5ef9c5ddbc27
WyattJ <jxq_ws@163.com>
2023-11-16 09:38:30 +0800

    s

--- src/N_m3u8DL-RE/Column/DownloadSpeedColumn.cs
+++ src/N_m3u8DL-RE/Column/DownloadSpeedColumn.cs
@@ -54,28 +54,8 @@ namespace N_m3u8DL_RE.Column
 
         public Text RenderDemo(ProgressTask task)
         {
-            var taskId = task.Id;
             var speedContainer = SpeedContainerDic[taskId];
-            var now = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             var flag = task.IsFinished || !task.IsStarted;
-            //单文件下载汇报进度
-            if (!flag && speedContainer.SingleSegment && speedContainer.ResponseLength != null)
-            {
-                task.MaxValue = (double)speedContainer.ResponseLength;
-                task.Value = speedContainer.RDownloaded;
-            }
-            //一秒汇报一次即可
-            if (DateTimeStringDic.TryGetValue(taskId, out var oldTime) && oldTime != now && !flag)
-            {
-                speedContainer.NowSpeed = speedContainer.Downloaded;
-                //速度为0，计数增加
-                if (speedContainer.Downloaded <= _stopSpeed) { speedContainer.AddLowSpeedCount(); }
-                else speedContainer.ResetLowSpeedCount();
-                speedContainer.Reset();
-                Logger.Info("<Speed>: "+FormatFileSize(speedContainer.NowSpeed) + (speedContainer.LowSpeedCount > 0 ? $"({speedContainer.LowSpeedCount})" : ""));
-            }
-            DateTimeStringDic[taskId] = now;
-            var style = flag ? Style.Plain : MyStyle;
             return flag ? "-" : FormatFileSize(speedContainer.NowSpeed) + (speedContainer.LowSpeedCount > 0 ? $"({speedContainer.LowSpeedCount})" : "");
         }
 
